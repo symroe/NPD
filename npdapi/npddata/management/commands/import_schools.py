d4c0dcab72524d35468c04d937cef8c22ba9bf0d
Sym Roe <sym.roe@talusdesign.co.uk>
2012-07-05 15:42:09 +0100

    Add more fields to schools

--- npdapi/npddata/management/commands/import_schools.py
+++ npdapi/npddata/management/commands/import_schools.py
@@ -11,20 +11,17 @@ SEEN = set()
 
 class Command(BaseCommand):
     
-    def guess_la_name(self, line, filename):
+    def guess_prefix(self, filename):
         file_name = os.path.split(filename)[-1]
-        try:
-            return float(line.get("%s_LA" % file_name[:3]))
-        except:
-            pass
-        
+        return file_name[:3]
+    
+    def get_with_prefix(self, fieldname, line):
+        return line.get('%s_%s' % (self.prefix, fieldname), '')
     
     def handle(self, path, **options):
         for filename in glob.glob("%s/*.txt" % path):
             in_file = csv.DictReader(open(filename), dialect='excel-tab')
             for line in in_file:
-                self.guess_la_name(line, filename)
-                pass
                 if line:
                     school_id = line['SCH_SCHOOLID']
                     if school_id not in SEEN:
@@ -32,7 +29,13 @@ class Command(BaseCommand):
                             s = School.objects.get(school_id=school_id)
                         except:
                             s = School(school_id=school_id)
-                    
+                        
+                        self.prefix = self.guess_prefix(filename)
+                        
+                        for k,v in line.items():
+                            del line[k]
+                            line[k.upper()] = v
+                        
                         s.name = line['SCH_SCHOOLNAME'].strip('"0b ')
                         s.address1 = line.get('SCH_ADDRESS1', '').strip()
                         s.address2 = line.get('SCH_ADDRESS2', '').strip()
@@ -40,7 +43,16 @@ class Command(BaseCommand):
                         s.town = line.get('SCH_TOWN', '').strip()
                         s.county = line.get('SCH_COUNTY', '').strip()
                         s.postcode = line.get('SCH_POSTCODE', '').strip()
-                        s.la = self.guess_la_name(line, filename)
+                        s.la = self.get_with_prefix('LA', line).strip("\" ")
+                        s.URN = self.get_with_prefix('URN', line).strip("\" ")
+                        s.establishment = int(self.get_with_prefix('ESTAB', line).strip("\" ") or 0)
+                        s.establishment_type = int(self.get_with_prefix('TOE_CODE', line).strip("\" ") or 0)
+                        s.nftype = line.get('SCH_NFTYPE').strip(' "')
                         s.save()
                         SEEN.add(school_id)
-    
+
+
+
+
+
+
--- npdapi/npddata/models.py
+++ npdapi/npddata/models.py
@@ -14,6 +14,24 @@ class School(models.Model):
     town = models.CharField(null=True, blank=True, max_length=255, help_text="SCH_TOWN")
     county = models.CharField(null=True, blank=True, max_length=255, help_text="SCH_COUNTY")
     postcode = models.CharField(null=True, blank=True, max_length=14, help_text="SCH_POSTCODE")
+    URN = models.IntegerField(blank=True, null=True, help_text="School's Unique Reference Number")
+    establishment = models.IntegerField(blank=True, null=True, help_text="Establishment number of the school attended as assigned by the DfE")
+    establishment_type = models.IntegerField(blank=True, null=True, help_text="Type of establishment code taken from Edubase.")
+    nftype = models.CharField(blank=True, max_length=100, help_text="School type")
+
+
+class PupilData(models.Model):
+    class meta:
+        abstract = True
+    
+    record_id = models.IntegerField(blank=False, null=False, primary_key=True, 
+            help_text="Unique record ID - A pseudonymised record ID required for internal purposes.  This ID will not facilitate linking across datasets.")
+    school = models.ForeignKey(School)
+    la = models.IntegerField(blank=True, null=True, 
+        help_text="""Local Authority (LA) that the school where the pupil 
+        attends reports to (original data).""")
+    gender = models.CharField(blank=True, max_length=1, help_text="KS[n]_GENDER")
+    
 
 
 
