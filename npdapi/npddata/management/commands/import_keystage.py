62aba20b5825d9bdbe5322f5603b6d2fa45494cb
Sym Roe <sym.roe@talusdesign.co.uk>
2012-07-06 12:00:37 +0100

    cleaned up importer, KS4 model

--- npdapi/npddata/management/commands/import_keystage.py
+++ npdapi/npddata/management/commands/import_keystage.py
@@ -5,7 +5,7 @@ import os
 
 from django.core.management.base import BaseCommand, CommandError
 
-from npddata.models import School, KS2
+from npddata.models import School, KS2, KS4, KS5
 
 schools = {}
 
@@ -18,39 +18,53 @@ class Command(BaseCommand):
     def get_with_prefix(self, fieldname, line):
         return line.get('%s_%s' % (self.prefix, fieldname), '')
     
-    
-    def import_ks2(self, in_file):
+    def clean_line(self, line):
+        for k,v in line.items():
+            if v:
+                v = v.strip(' "')
+            if not v:
+                v = None
+            del line[k]
+            line[k[4:]] = v
+        
+        if '2BTECEDEXDA' in line.keys():
+            tmp_v = line['2BTECEDEXDA']
+            del line['2BTECEDEXDA']
+            line['TWOBTECEDEXDA'] = tmp_v
+        
+        return line
+
+    def import_for_ks(self, model, in_file):
         for line in in_file:
             if line:
-                r = KS2(
+                r = model(
                     record_id=self.get_with_prefix('RECORDID', line),
                     school_id = line.get('SCH_SCHOOLID'),
                     la = self.get_with_prefix('LA', line).strip(' "'),
                     gender = self.get_with_prefix('GENDER', line).strip(' "'),
                     year = self.year,
                 )
-                
-                for k,v in line.items():
-                    if v:
-                        v = v.strip(' "')
-                    del line[k]
-                    line[k[4:]] = v
+                line = self.clean_line(line)
                 r.__dict__.update(line)
                 
                 r.save()
-                    
-        
-    
-    
+
     def handle(self, keystage, path, **options):
         for filename in glob.glob("%s/KS%s_*.txt" % (path, keystage)):
             in_file = csv.DictReader(open(filename), dialect='excel-tab')
             self.prefix = "KS%s" % keystage
             self.year = file_name = os.path.split(filename)[-1][4:8]
+                        
             if int(keystage) == 2:
-                self.import_ks2(in_file)
+                model = KS2
 
+            if int(keystage) == 4:
+                model = KS4
 
+            if int(keystage) == 5:
+                model = KS5
+
+            self.import_for_ks(model, in_file)
 
 
 
