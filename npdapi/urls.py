0da7540feb0672fbc46f2ce1e89ba8ef723c325c
Sym Roe <sym.roe@talusdesign.co.uk>
2012-07-07 11:48:23 +0100

    add endpoint URLS

--- npdapi/urls.py
+++ npdapi/urls.py
@@ -6,13 +6,15 @@ admin.autodiscover()
 
 from tastypie.api import Api
 
-from api.api import EntryResource, SchoolResource, KS2Resource
+from api.api import EntryResource, SchoolResource, KS2Resource, KS4Resource, KS5Resource
 
 
 v1_api = Api(api_name='v1')
 v1_api.register(EntryResource())
 v1_api.register(SchoolResource())
 v1_api.register(KS2Resource())
+v1_api.register(KS4Resource())
+v1_api.register(KS5Resource())
 
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
